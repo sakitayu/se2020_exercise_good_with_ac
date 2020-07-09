a1f3303be669716440d45e33a4ed8067a1ee6b52
sakitayu <sakitakitakita@gmail.com>
2020-07-09 13:31:37 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root to:'application#home'
-  end
+  root to:'application#home'
+
 end
